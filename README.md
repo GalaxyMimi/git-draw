a6629d5452a3df9abcfec524c10c595ebe4d179f
StevenJoeZhang <1119186082@qq.com>
2018-04-28 16:55:23 +0800

    updated README.md

--- README.md
+++ README.md
@@ -1,19 +1,39 @@
 # Git Draw
-Allows you to draw in your GitHub heatmap
+Allows you to draw in your GitHub heatmap.
 
-![demo-vid](\media/demo-video.gif)
+![demo-video](media/demo-video.gif)
 
-##### Created by Ben Friedland - http://www.bugben.com
-##### Improved by Shuqiao Zhang - http://zsq.im
+#### Created by Ben Friedland - http://www.bugben.com
+#### Improved by Shuqiao Zhang - http://zsq.im
 
 This is a HTML and JavaScript project which will allow you to freely draw on a GitHub heatmap. You can then export your drawing to a shell script containing a git commit log. Once you've run and pushed this script to a new repository, your commit log will match the drawing you made.
 
+## Install
 Download the project here: 
 ```
 git clone https://github.com/stevenjoezhang/git-draw.git
 cd git-draw/src
 ```
-Then open `index.html` using your browser. Safari, Chrome and FireFox are all supported.
+
+Then open `index.html` with your browser. Safari, Chrome and FireFox are all supported.
+
+## Demo
+
+[Git Draw](https://galaxymimi.com/app/gitdraw)
+
+## Usage
+
+- Draw in the GitHub heatmap.
+- Click "Download Script..." to save the shell script (git-draw.sh).
+- Create a empty repository on GitHub.
+- Run this:
+```
+sudo chmod +x git-draw.sh
+./git-draw.sh
+cd git-drawing
+git remote add origin https://github.com/your_username/your_repo.git
+git push -u origin master
+```
 
 ## Instructional Video
 
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,29 +1,27 @@
-/*jslint browser: true*/
-/*global Promise, chrome*/
 "use strict";
 
-var gf = {
+var gitDraw = {
 	init: function() {
 		for (var i = 0; i <= 52; i++) {
 			var trans = 13 * i;
 			var x = 13 - i;
-			$("#main").prepend('\
+			$("#main").append('\
 					<g transform="translate(' + trans + ', 0)">\
-						<rect class="day" width="10" height="10" x="' + x + '" y="0" fill="#ebedf0" data-count="0" data-date="' + gf.calcDate(i, 0) + '"></rect>\
-						<rect class="day" width="10" height="10" x="' + x + '" y="12" fill="#ebedf0" data-count="0" data-date="' + gf.calcDate(i, 1) + '"></rect>\
-						<rect class="day" width="10" height="10" x="' + x + '" y="24" fill="#ebedf0" data-count="0" data-date="' + gf.calcDate(i, 2) + '"></rect>\
-						<rect class="day" width="10" height="10" x="' + x + '" y="36" fill="#ebedf0" data-count="0" data-date="' + gf.calcDate(i, 3) + '"></rect>\
-						<rect class="day" width="10" height="10" x="' + x + '" y="48" fill="#ebedf0" data-count="0" data-date="' + gf.calcDate(i, 4) + '"></rect>\
-						<rect class="day" width="10" height="10" x="' + x + '" y="60" fill="#ebedf0" data-count="0" data-date="' + gf.calcDate(i, 5) + '"></rect>\
-						<rect class="day" width="10" height="10" x="' + x + '" y="72" fill="#ebedf0" data-count="0" data-date="' + gf.calcDate(i, 6) + '"></rect>\
+						<rect class="day" width="10" height="10" x="' + x + '" y="0" fill="#ebedf0" data-count="0" data-date="' + gitDraw.calcDate(i, 0) + '"></rect>\
+						<rect class="day" width="10" height="10" x="' + x + '" y="12" fill="#ebedf0" data-count="0" data-date="' + gitDraw.calcDate(i, 1) + '"></rect>\
+						<rect class="day" width="10" height="10" x="' + x + '" y="24" fill="#ebedf0" data-count="0" data-date="' + gitDraw.calcDate(i, 2) + '"></rect>\
+						<rect class="day" width="10" height="10" x="' + x + '" y="36" fill="#ebedf0" data-count="0" data-date="' + gitDraw.calcDate(i, 3) + '"></rect>\
+						<rect class="day" width="10" height="10" x="' + x + '" y="48" fill="#ebedf0" data-count="0" data-date="' + gitDraw.calcDate(i, 4) + '"></rect>\
+						<rect class="day" width="10" height="10" x="' + x + '" y="60" fill="#ebedf0" data-count="0" data-date="' + gitDraw.calcDate(i, 5) + '"></rect>\
+						<rect class="day" width="10" height="10" x="' + x + '" y="72" fill="#ebedf0" data-count="0" data-date="' + gitDraw.calcDate(i, 6) + '"></rect>\
 					</g>');
 		}
 		$(".month").each(function(i, item) {
-			$(item).attr("x", gf.positionArray[i]);
+			$(item).attr("x", gitDraw.positionArray[i]);
 		});
 		$("#father").html($("#father").html()); // force refresh svg
 		$("rect[data-count]").each(function(i, item) {
-			gf.maxCommits = Math.max(gf.maxCommits, item.getAttribute("data-count"));
+			gitDraw.maxCommits = Math.max(gitDraw.maxCommits, item.getAttribute("data-count"));
 			$(item).hover(function() {
 				var r = this.getAttribute("data-date");
 				var a = (function() {
@@ -44,18 +42,18 @@ var gf = {
 				$(".svg-tip").remove();
 			});
 		});
-		gf.commitBlockSize = Math.ceil(gf.maxCommits / 4);
-		$(".contrib-legend").unbind("click").on('mousedown', gf.setBrush);
-		$("svg rect").unbind("click").on("mousedown", gf.colorCell);
-		$("svg rect").on("mouseover", gf.cellOver);
+		gitDraw.commitBlockSize = Math.ceil(gitDraw.maxCommits / 4);
+		$(".contrib-legend").unbind("click").on('mousedown', gitDraw.setBrush);
+		$("svg rect").unbind("click").on("mousedown", gitDraw.colorCell);
+		$("svg rect").on("mouseover", gitDraw.cellOver);
 		var btnGroupNode = $("<div>").addClass("btn-group");
 		var renderBtnNode = $("<button>").text("Render").addClass("btn").addClass("btn-sm");
 		var downloadBtnNode = $(renderBtnNode).clone().text("");
 		var downloadTextNode = $("<span>").text(" Download Script...");
 		var downloadIconNode = $("<span>").addClass("octicon").addClass("octicon-desktop-download");
 		$(downloadBtnNode).append(downloadIconNode).append(downloadTextNode);
-		$(renderBtnNode).click(gf.output);
-		$(downloadBtnNode).click(gf.save);
+		$(renderBtnNode).click(gitDraw.output);
+		$(downloadBtnNode).click(gitDraw.save);
 		$(btnGroupNode).append(downloadBtnNode).append(renderBtnNode);
 		$(".contrib-legend").html(function(index, html) {
 			return html.replace('Less', 'Brush Colors');
@@ -72,7 +70,7 @@ var gf = {
 			var year = date.getFullYear();
 			var month = (date.getMonth() + 1).toString();
 			var day = (date.getDate()).toString();
-			if (day == "1") gf.positionArray[month - 1] = 40 + 12 * i;
+			if (day == "1") gitDraw.positionArray[month - 1] = 36 + 12 * i;
 			if (month.length == 1) {
 				month = "0" + month;
 			}
@@ -80,7 +78,7 @@ var gf = {
 				day = "0" + day;
 			}
 			return year + "-" + month + "-" + day;
-		}//将日期转化字符串(yyyy-MM-dd)
+		} // 将日期转化字符串(yyyy-MM-dd)
 
 		function getDay(i, j) {
 			var date = new Date();
@@ -90,44 +88,44 @@ var gf = {
 		return DateToStr(getDay(i, j));
 	},
 	brushColor: function() {
-		return gf.brushMappings[gf.shade];
+		return gitDraw.brushMappings[gitDraw.shade];
 	},
 	setBrush: function(e) {
 		$(".legend>li").removeClass("selected");
-		gf.shade = $(e.target).index();
+		gitDraw.shade = $(e.target).index();
 		$(e.target).addClass("selected");
 	},
 	colorCell: function(e) {
-		$(e.target).attr("fill", gf.brushColor())
+		$(e.target).attr("fill", gitDraw.brushColor())
 			.attr("data-touched", "true")
-			.attr("data-target-shade", gf.shade);
+			.attr("data-target-shade", gitDraw.shade);
 	},
 	cellOver: function(e) {
 		// mouse drag
 		if (e.buttons == 1) {
-			gf.colorCell(e);
+			gitDraw.colorCell(e);
 		}
 	},
 	render: function() {
-		gf.script = "#!/bin/bash\n\n" +
-			"REPO=gf\n" +
+		gitDraw.script = "#!/bin/bash\n\n" +
+			"REPO=git-drawing\n" +
 			"git init $REPO\n" +
 			"cd $REPO\n" +
 			"echo \"Created with Git Draw (http://github.com/stevenjoezhang/git-draw)\" > README.md\n" +
 			"git add README.md\n" +
-			"touch gf\n" +
-			"git add gf\n" +
-			"echo 0 > gf\n";
+			"touch git-drawing\n" +
+			"git add git-drawing\n" +
+			"echo 0 > git-drawing\n";
 		$("svg rect").each(function(i, item) {
 			if (!item.hasAttribute("data-touched")) {
 				return true; // read as "continue"
 			}
-			var targetShade = gf.brushMappings.indexOf($(item).attr("fill"));
+			var targetShade = gitDraw.brushMappings.indexOf($(item).attr("fill"));
 			if (targetShade == -1) {
 				targetShade = 0;
 			}
 			var existingCommitCount = $(item).attr("data-count");
-			var targetCommitCount = targetShade * gf.commitBlockSize;
+			var targetCommitCount = targetShade * gitDraw.commitBlockSize;
 			var commitsToAdd = targetCommitCount - existingCommitCount;
 			var dateStr = $(item).attr("data-date");
 			var canvas = $('svg')[0]
@@ -135,14 +133,14 @@ var gf = {
 				return false;
 			}
 			for (var j = 0; j < commitsToAdd; j++) {
-				gf.script += "GIT_AUTHOR_DATE=" + dateStr + "T12:00:00 GIT_COMMITTER_DATE=" + dateStr + "T12:00:00 git commit -a -m \"gf\" > /dev/null\n"
-				gf.script += "echo " + i % 2 + "-" + j % 2 + " > gf\n";
+				gitDraw.script += "GIT_AUTHOR_DATE=" + dateStr + "T12:00:00 GIT_COMMITTER_DATE=" + dateStr + "T12:00:00 git commit -a -m \"Git Draw\" > /dev/null\n"
+				gitDraw.script += "echo " + i % 2 + "-" + j % 2 + " > git-drawing\n";
 			}
 		});
 	},
 	output: function() {
-		gf.render();
-		$(".activity-listing").html("<pre><code>" + gf.script + "</code></pre>")
+		gitDraw.render();
+		$(".activity-listing").html("<pre><code>" + gitDraw.script + "</code></pre>")
 			.css("font-size", "8px")
 			.css("color", "#FFE")
 			.css("background-color", "#333")
@@ -151,13 +149,13 @@ var gf = {
 		var downloadTextNode = $("<span>").text(" Download Script...");
 		var downloadIconNode = $("<span>").addClass("octicon").addClass("octicon-desktop-download");
 		$(downloadBtnNode).append(downloadIconNode).append(downloadTextNode).css("margin", "10px 0px");
-		$(downloadBtnNode).click(gf.save);
+		$(downloadBtnNode).click(gitDraw.save);
 		$(".activity-listing").append(downloadBtnNode);
 	},
 	save: function() {
-		gf.render();
+		gitDraw.render();
 		// creates an invisible download link and clicks it
-		var textFileAsBlob = new Blob([gf.script], {
+		var textFileAsBlob = new Blob([gitDraw.script], {
 			type: 'text/plain'
 		});
 		var fileNameToSaveAs = 'git-draw.sh';
@@ -176,4 +174,4 @@ var gf = {
 	}
 }
 
-gf.init();
+gitDraw.init();
